13f9928f39a48dd31352a0c69b24bc15b6655c29
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-09-24 15:49:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -46,9 +46,13 @@ function notify() {
 	var name1 = document.getElementById('name1').value;
 	var name = document.getElementById('name').value;
 	var phone = document.getElementById('phone').value;
+	var tyme = document.getElementById('tyme').value;
 	var arr = name1.split(" ");
 	if (arr.length == 1) {
 		document.getElementById('message').innerHTML = "Please enter you full name";
+	}
+	else if(name == "" || phone=="" || tyme==""){
+		document.getElementById('message').innerHTML = "All fields are required";
 	}
 	else{
 	var request = new XMLHttpRequest();
@@ -57,19 +61,22 @@ function notify() {
 			//alert(this.responseText);
 			if (this.responseText=="1") {
 				document.getElementById('message').innerHTML = "Notification sent!";
-$('#myModal').modal('show');
+
 			}
 			else{
 				//document.getElementById('message').innerHTML = "Unable to notify";
 				document.getElementById('message').innerHTML = "Notification sent!";
-$('#myModal').modal('show');
-			}
+				document.getElementById('name1').value = "";
+document.getElementById('name').value = "";
+document.getElementById('phone').value = "";
 		}
+	}
 	};
-	request.open("GET","functions.php?uname="+name+"&phone="+phone+"&mname="+name1, true);
+	request.open("GET","functions.php?uname="+name+"&phone="+phone+"&mname="+name1+"&tyme="+tyme, true);
 	request.send();
 }
 }
+
 
 function fetch() {
 	var request = new XMLHttpRequest();
@@ -86,4 +93,10 @@ function fetch() {
 
 function move(){
 window.location.assign("index.html");
+}
+
+function back() {
+
+history.go(-(history.length - 1));
+
 }
