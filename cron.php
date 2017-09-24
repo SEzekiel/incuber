445d492bf81685c0f4f37ebf265647e5186f3545
Ezekiel Sebastine <SEzekiel@users.noreply.github.com>
2017-09-24 16:10:30 +0000

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,11 +1,16 @@
 <?php
 
-
+/**
+*This script will handle the removal of expired post
+*it will be executed by the server every minute
+*Cron needs to be enabled on your server for this to work
+**/
 fetch();
 function fetch()
 {
 
-	require('http://incuber.spleint.com/auth.php');
+	//TO DO: Include a connection to your database here first
+	//$con = mysqli_connect("host","user","password","database");
 	$query = "SELECT * FROM ubers WHERE 1";
 
 	$result = mysqli_query($conn,$query);
@@ -13,13 +18,24 @@ function fetch()
 	if (mysqli_num_rows($result) > 0) {
 		 while($row = mysqli_fetch_assoc($result)){
 		 	$id = $row['id'];
-		 	$minutes = $row['time'];
-		 	$minutes = (int)((time()-strtotime($minutes))/60);
+		 	$tyme= $row['tyme'];
+		 	//$minutes = (int)((time()-strtotime($minutes))/60);
 			
-			if ($minutes >= 60) {
+			if ($tyme <=0) {
 				$qry = "DELETE FROM ubers WHERE id ='$id'";
-
 				$rslt = mysqli_query($conn,$qry);
+			}
+			else{
+				if(($tyme-2)<0){
+					$tyme = $tyme - 1;
+					$qry = "UPDATE ubers SET tyme = '$tyme' WHERE id ='$id'";
+					$rslt = mysqli_query($conn,$qry);
+				}
+				else{
+					$tyme = $tyme - 2;
+					$qry = "UPDATE ubers SET tyme = '$tyme' WHERE id ='$id'";
+					$rslt = mysqli_query($conn,$qry);
+				}
 			}
 		}
 	}
